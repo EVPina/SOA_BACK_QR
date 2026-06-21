40ead358f1f9a043f8ab6f32664a837e2d72aba9
enzo <enzo.vp@outlook.com>
2026-06-20 20:11:36 -0500

    cambios de controlador

--- src/main/java/com/donbelisario/menudigital/controller/MenuController.java
+++ src/main/java/com/donbelisario/menudigital/controller/MenuController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/menu")
+@RequestMapping("api/v1/menu")
 @CrossOrigin(origins = "*")
 public class MenuController {
     
